638078ba6d6926e7b952ac22daa3bc120c1a9ec9
mk <midaslmg94@gmail.com>
2020-02-06 14:45:13 +0900

    상태 컴포넌트 - 화면 갱신표시

--- day01/hello-react2/src/Counter.js
+++ day01/hello-react2/src/Counter.js
@@ -2,15 +2,22 @@ import React from 'react';
 class Counter extends React.Component{
     // state 사용하자
     state={
-        number: 1
+        number: 100
+    };
+    clickToIncrease=()=>{
+        this.setState({number:this.state.number+1})
+    };
+    clickToDecrease=()=>{
+        this.setState({number:this.state.number-1}) // 화면에 변하는 상태를 출력
+        this.state.number=this.state.number-1; // setState를 안쓰면 값은 바뀔지라도 화면에 바뀌는 모습이 보이지 않는다. 상태변수의 값은 setState 메소드를 통해 쓰자
     };
     render(){
         return(
             <div>   
                 <h1>카운터</h1>
                 <div>값:{this.state.number}</div>
-                <button>더하기 버튼</button>
-                <button>빼기 버튼</button>             
+                <button onClick={this.clickToIncrease}>더하기 버튼</button>
+                <button onClick={this.clickToDecrease}>빼기 버튼</button>             
             </div>
         );
     }
