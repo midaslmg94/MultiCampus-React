e51ae03998ac597d84bee117b7b949e22b03f44d
mk <midaslmg94@gmail.com>
2020-02-07 09:36:20 +0900

    자식 컴포넌트에서 불필요한 렌더링 발생

--- day01/hello-react2/src/Title.js
+++ day01/hello-react2/src/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
 function Title(props) {
+    console.log(props);
     return <p>{props.title}</p>
 }
 export default Title;
--- day01/hello-react2/src/Todo.js
+++ day01/hello-react2/src/Todo.js
@@ -2,19 +2,26 @@ import React, {Fragment} from 'react';
 import Title from './Title';
 class TODO extends React.Component {
     state = {
-        count: 0
+        count1: 0, count2:0
     };
-    onClick = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
-    };
+   onClick1=()=>{
+    this.setState({count1:this.state.count1+1});
+   }
+   onClick2=()=>{
+    this.setState({count2:this.state.count2+2});
+   }
     render() {
         return (
-            <Fragment>
-                <Title title={`현재 카운트는 ${this.state.count}입니다`}/>
-                <button onClick={this.onClick}>증가</button>
-            </Fragment>
+            <div>
+                <Title title={`현재 카운트는 ${this.state.count1}입니다`}/>
+                <p>{this.state.count1}</p>
+                <button onClick={this.onClick1}>증가1</button>
+                <button onClick={this.onClick2}>증가2</button> 
+                {/*증가 2를 눌렀을 때, 자식 컴포넌트도 함께 렌더링 된다.
+                ==> 불필요한 렌더링 발생(화면에 카운트가 증가되지는 않지만, 콘솔창에서 찍힘)
+                이러한 불필요함을 방지하기 위해서는 React.memo 또는 React.PureComponent를 사용
+                */}
+            </div>
         );
     }
 }
